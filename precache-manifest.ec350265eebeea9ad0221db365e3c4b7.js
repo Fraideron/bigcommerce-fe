1e2fe80e2e540a925e1ba6e1e99b371d3f6c9893
valeriy <jiafen24@gmail.com>
2020-07-18 22:24:24 +0300

    Updates

---
+++ precache-manifest.ec350265eebeea9ad0221db365e3c4b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76757513b6eae37039d6208f90bdfd50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88755f6a5f2b68d6f66e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6056722903e2d5a70ff2",
+    "url": "/static/js/2.0bc9b7d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0bc9b7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88755f6a5f2b68d6f66e",
+    "url": "/static/js/main.9d14d143.chunk.js"
+  },
+  {
+    "revision": "becdf6f7076d0cdc591d",
+    "url": "/static/js/runtime-main.caac981b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
