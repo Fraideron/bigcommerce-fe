ccd0cad47cc4d250c3a801fc99c807e8d107ffc0
valeriy <jiafen24@gmail.com>
2020-07-18 22:37:02 +0300

    Updates

---
+++ precache-manifest.55bbb5032a9b594ded35f8184d473222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04fab14b006405d3b4b0c4be530bba2b",
+    "url": "/bigcommerce-fe/index.html"
+  },
+  {
+    "revision": "3c4e6d20d93706f8fd20",
+    "url": "/bigcommerce-fe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6056722903e2d5a70ff2",
+    "url": "/bigcommerce-fe/static/js/2.0bc9b7d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bigcommerce-fe/static/js/2.0bc9b7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4e6d20d93706f8fd20",
+    "url": "/bigcommerce-fe/static/js/main.186a26e0.chunk.js"
+  },
+  {
+    "revision": "a17dccac9c3a37108825",
+    "url": "/bigcommerce-fe/static/js/runtime-main.3fcc43bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bigcommerce-fe/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec350265eebeea9ad0221db365e3c4b7.js"
+  "/bigcommerce-fe/precache-manifest.55bbb5032a9b594ded35f8184d473222.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bigcommerce-fe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
